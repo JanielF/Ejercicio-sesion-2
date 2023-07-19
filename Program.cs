6a638440ffa394411a9fa5f19694d60e46958b1a
JanielF <janielfloresdo@gmail.com>
2023-07-19 17:43:56 -0400

    Todos los ejercicios realizados

--- Program.cs
+++ Program.cs
@@ -28,3 +28,71 @@ else
 {
     Console.WriteLine("Hola buenas mi nombre es: " + nombre + " mi apellido es: " + apellido + " y mi edad es: " + edad + ", Actualmente:  No se programar");
 }
+
+//ejercico 2
+
+//Tipos Usando los tipos de variables más adecuados, describe los objetos siguientes:
+
+//Coche: puertas, ruedas, marca, ITV vigente
+
+//Mesa: peso, largo, material, color
+
+//Nota: puedes escribir estos datos por consola si prefieres verlos. La idea del ejercicio es almacenar los datos en los tipos más adecuados.
+
+//Coche
+int puertas;
+int ruedas;
+string marca;
+bool ITVVigente;
+
+//Mesa
+long peso;
+float largo;
+string material;
+string color;
+
+//Nota: puedes escribir estos datos por consola si prefieres verlos. La idea del ejercicio es almacenar los datos en los tipos más adecuados.
+
+
+/*
+ 
+Ejercicio 3
+
+Operadores Determina los operadores para verificar las siguientes condiciones:
+
+Un número es mayor o igual a 18
+
+Un char es ‘a’
+
+Se cumplen dos conciones a la vez (ambas verdaderas)
+
+Se cumple una de dos condiciones a la vez (una true y otra false)
+
+Nota: puedes escribir estos datos por consola si prefieres verlos. La idea del ejercicio es almacenar los datos en los tipos más adecuados.
+*/
+
+int numero = 10;
+
+if (numero >= 18)
+{
+
+}
+
+char a;
+
+int numero1 = 10;
+
+if (numero1 <= 10 && numero <= 10)
+{
+
+}
+else
+{
+
+}
+
+if (numero > 20 || numero1 > 5)
+{
+
+}
+
